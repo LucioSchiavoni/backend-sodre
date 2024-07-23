5743f81ae20ad01a5f708e084cde5aec9ae58d7c
Lucio B. Schiavoni Lafourcade <lucio.schiavoni@mec.local>
2024-07-23 10:08:49 -0300

    feat(): sendEmail

--- src/middlewares/sendEmail.js
+++ src/middlewares/sendEmail.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv';
+import prisma from '../config/db.js';
 dotenv.config();
 
     const transporter =  nodemailer.createTransport({
@@ -15,24 +16,38 @@ dotenv.config();
 
  const sendEmailService = async(req,res) => {
 
-    const {nombre,email} = req.body;
+    const {eventoId} = req.body;
 
+    const ganadores = await prisma.ganador.findMany({
+        where:{
+            eventoId: eventoId
+        },
+        include:{
+            usuario: true
+        }
+    })
+
+    const listaGanadores = ganadores.map( g => `<div style="padding: 8px; border-bottom: 1px solid #ddd; font-size: 16px; font-weight: bold;">${g.usuario.nombre}</div>`).join('');
 
     const mailOptions = {
         from: process.env.EMAIL_USER,
-        to: `${email}`,
-        subject: `Ganador del sorteo`,
+        to: process.env.EMAIL_TO,
+        subject: `Ganadores del sorteo`,
         html: `
         <div>
-        <h1>Buenas tardes ${nombre}</h2>
-        <p>Por este medio te comunicamos que fuiste seleccionado para asistir a la función del sodre</p>
+        <h2>Buenas tardes</h2>
+        <p>Nos complace informar que el grupo de ganadores del sorteo es el siguiente:</p>
+        <div style="display: flex; flex-direction: column;">
+         ${listaGanadores}
+        </div>
+        <p>Por este medio les comunicamos que han sido seleccionados para asistir a la función del Sodre.</p>
         <p>En caso de no poder asistir, por favor responder a este correo.</p>
         </div>
         `
     }  
     try {
         const info = await transporter.sendMail(mailOptions);
-        return res.json({message: 'Se envio un correo al usuario'})
+        return res.json({message: 'Se envio un correo notificando los ganadores'})
     } catch (error) {
         console.log(error)
     }
